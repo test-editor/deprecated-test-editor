f333332899f35aa446a041d45b9411f14dcc5be8
Karsten Panier <kpanier@aysada.org>
2015-03-19 22:58:51 +0100

    TE-113: Highlight Log Entries in TestLogview * Adding a configure Button to the Viewpart
    Task-Url: https://testeditor.atlassian.net/browse/TE-113

--- ui/org.testeditor.ui.test/src/org/testeditor/ui/parts/reporting/TestLogViewTest.java
+++ ui/org.testeditor.ui.test/src/org/testeditor/ui/parts/reporting/TestLogViewTest.java
@@ -1,11 +1,13 @@
 package org.testeditor.ui.parts.reporting;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.eclipse.e4.core.contexts.ContextInjectionFactory;
 import org.eclipse.e4.core.contexts.EclipseContextFactory;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.swt.custom.StyleRange;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.junit.After;
@@ -20,6 +22,7 @@ import org.testeditor.core.services.interfaces.TestEditorPlugInService;
 import org.testeditor.core.services.interfaces.TestStructureService;
 import org.testeditor.ui.adapter.MPartAdapter;
 import org.testeditor.ui.adapter.TestStructureServiceAdapter;
+import org.testeditor.ui.constants.ColorConstants;
 import org.testeditor.ui.mocks.TestEditorPluginServiceMock;
 
 /**
@@ -62,6 +65,23 @@ public class TestLogViewTest {
 		testCase.setName("MyName");
 		testLogView.onTestExecutionShowTestLogForLastRun(testCase);
 		assertTrue(part.getLabel().endsWith(testCase.getName()));
+	}
+
+	/**
+	 * Tests the style ranges creation based on a testlog entry.
+	 * 
+	 */
+	@Test
+	public void testGetStyleRanges() {
+		TestLogView logView = new TestLogView(null);
+		String logData = "No Style\nINFO Range\nERROR Range\nINFO another info\nTRACE Range\nDEBUG Range";
+		StyleRange[] styleRanges = logView.getStyleRanges(logData);
+		assertEquals(5, styleRanges.length);
+		assertTrue(styleRanges[0].foreground.equals(ColorConstants.COLOR_DARK_GRAY));
+		assertTrue(styleRanges[1].foreground.equals(ColorConstants.COLOR_RED));
+		assertTrue(styleRanges[2].foreground.equals(ColorConstants.COLOR_DARK_GRAY));
+		assertTrue(styleRanges[3].foreground.equals(ColorConstants.COLOR_DARK_GREEN));
+		assertTrue(styleRanges[4].foreground.equals(ColorConstants.COLOR_BLUE));
 	}
 
 	/**
