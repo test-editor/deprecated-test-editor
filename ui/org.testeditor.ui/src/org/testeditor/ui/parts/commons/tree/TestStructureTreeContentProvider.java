e7671da13ef992c474177b437843ffef1f2a3941
ocharlet <oliver.charlet@clarities.de>
2015-04-22 16:42:10 +0200

    [TE-86] refactoring TestStructureTreeModel

--- ui/org.testeditor.ui/src/org/testeditor/ui/parts/commons/tree/TestStructureTreeContentProvider.java
+++ ui/org.testeditor.ui/src/org/testeditor/ui/parts/commons/tree/TestStructureTreeContentProvider.java
@@ -27,7 +27,7 @@ import org.testeditor.core.model.teststructure.TestCompositeStructure;
 import org.testeditor.core.model.teststructure.TestProject;
 import org.testeditor.core.model.teststructure.TestStructure;
 import org.testeditor.core.services.interfaces.TestProjectService;
-import org.testeditor.core.services.interfaces.TestStructureTreeModelService;
+import org.testeditor.core.services.interfaces.TestStructureTreeModel;
 
 /**
  * 
@@ -45,7 +45,7 @@ public class TestStructureTreeContentProvider implements ITreeContentProvider {
 
 	@Override
 	public Object[] getElements(Object inputElement) {
-		TestStructureTreeModelService treeInputService = (TestStructureTreeModelService) inputElement;
+		TestStructureTreeModel treeInputService = (TestStructureTreeModel) inputElement;
 		List<? extends TestStructure> elements = new ArrayList<TestStructure>();
 		try {
 			elements = treeInputService.getElements();
