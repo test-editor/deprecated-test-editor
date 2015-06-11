d378faa65f9417cbb57cdf2529bef3fe3dd51a2c
Orhan Polat <orhan.polat@akquinet.de>
2015-06-11 08:36:31 +0200

    [TE-126] add FileWatcherService for notify library changes

---
+++ core/org.testeditor.core/src/org/testeditor/core/services/impl/FileWatchServiceImpl.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2012 - 2014 Signal Iduna Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Signal Iduna Corporation - initial API and implementation
+ * akquinet AG
+ *******************************************************************************/
+package org.testeditor.core.services.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.testeditor.core.model.teststructure.TestProject;
+import org.testeditor.core.services.interfaces.FileWatchService;
+
+public class FileWatchServiceImpl implements FileWatchService {
+
+	Map<TestProject, FileWatcher> pool = new HashMap<TestProject, FileWatcher>();
+
+	@Override
+	public void watch(TestProject testProject) {
+
+		if (!pool.containsKey(testProject)) {
+			FileWatcher fileWatcher = new FileWatcher(testProject);
+			pool.put(testProject, fileWatcher);
+			try {
+				fileWatcher.watch();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
