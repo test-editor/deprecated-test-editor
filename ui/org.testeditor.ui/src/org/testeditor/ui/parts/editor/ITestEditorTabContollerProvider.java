e94fca3d3f8d53a7a3d7f654931925e42d715537
Georg <georg.portwich@signal-iduna.de>
2015-06-29 16:39:22 +0200

    Getting the TestEditorMetaDataTabController from a provider / update for MetaData implemented

---
+++ ui/org.testeditor.ui/src/org/testeditor/ui/parts/editor/ITestEditorTabContollerProvider.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2012 - 2015 Signal Iduna Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Signal Iduna Corporation - initial API and implementation
+ * akquinet AG
+ *******************************************************************************/
+package org.testeditor.ui.parts.editor;
+
+import javax.inject.Provider;
+
+/**
+ * Provider to create an additional Tab for the TestEditor.
+ */
+public interface ITestEditorTabContollerProvider extends Provider<ITestEditorTabController> {
+}
