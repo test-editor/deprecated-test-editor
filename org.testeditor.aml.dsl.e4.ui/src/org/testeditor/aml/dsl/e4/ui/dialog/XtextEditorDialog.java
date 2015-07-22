501d74e8bfdff27cea95e08c0931279b278f0a30
Franz Becker <franz.becker@itemis.de>
2015-07-22 18:56:33 +0200

    [TE-206] organize imports

--- org.testeditor.aml.dsl.e4.ui/src/org/testeditor/aml/dsl/e4/ui/dialog/XtextEditorDialog.java
+++ org.testeditor.aml.dsl.e4.ui/src/org/testeditor/aml/dsl/e4/ui/dialog/XtextEditorDialog.java
@@ -9,7 +9,6 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.resource.XtextResourceSet;
