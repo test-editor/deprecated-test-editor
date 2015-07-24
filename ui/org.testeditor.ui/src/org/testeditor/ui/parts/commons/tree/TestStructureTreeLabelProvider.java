b68ef5d4dcc8bfbeb7163062edaab1b784171e51
Karsten Panier <kpanier@aysada.org>
2015-07-24 08:39:24 +0200

    TE-233: No execution result Icon on executed tests
    - Checking execution state on unmodified objects
    
    Task-Url: https://testeditor.atlassian.net/browse/TE-233

--- ui/org.testeditor.ui/src/org/testeditor/ui/parts/commons/tree/TestStructureTreeLabelProvider.java
+++ ui/org.testeditor.ui/src/org/testeditor/ui/parts/commons/tree/TestStructureTreeLabelProvider.java
@@ -16,7 +16,6 @@ import javax.inject.Inject;
 import org.eclipse.jface.viewers.ILabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
-import org.testeditor.core.model.team.TeamChangeType;
 import org.testeditor.core.model.testresult.TestResult;
 import org.testeditor.core.model.teststructure.BrokenTestStructure;
 import org.testeditor.core.model.teststructure.ScenarioSuite;
@@ -148,6 +147,12 @@ public class TestStructureTreeLabelProvider extends LabelProvider implements ILa
 						return IconConstants.ICON_TESTCASE_SUCCESSED_MODIFIED;
 					} else {
 						return IconConstants.ICON_TESTCASE_FAILED_MODIFIED;
+					}
+				} else {
+					if (testResult.isSuccessfully()) {
+						return IconConstants.ICON_TESTCASE_SUCCESSED;
+					} else {
+						return IconConstants.ICON_TESTCASE_FAILED;
 					}
 				}
 
