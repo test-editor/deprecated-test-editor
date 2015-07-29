2f721783310c4291d49a98fa29d78207579013db
Orhan Polat <orhan.polat@akquinet.de>
2015-07-29 12:45:56 +0200

    TE-242 Menuitems in Team Kontextmenu are disabled

--- ui/org.testeditor.ui/src/org/testeditor/ui/handlers/teamshare/RevertElementHandler.java
+++ ui/org.testeditor.ui/src/org/testeditor/ui/handlers/teamshare/RevertElementHandler.java
@@ -86,7 +86,7 @@ public class RevertElementHandler extends AbstractUpdateOrApproveHandler {
 		WizardDialog wizardDialog = new WizardDialog(shell, nwiz);
 
 		if (wizardDialog.open() == Window.OK) {
-			super.execute(context.get(IEventBroker.class));
+			super.execute(context.get(IEventBroker.class), context);
 		}
 	}
 
